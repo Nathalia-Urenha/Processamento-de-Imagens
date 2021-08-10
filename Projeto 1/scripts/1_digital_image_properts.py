42fdfb8dcdc1cb173e69cc4916ba03aa3f8f623a
Nathalia-Urenha <naty.urenha@gmail.com>
2021-08-09 22:44:32 -0300

    Inversão de Pixels; Conversão de RGB p/ Gray Scale

---
+++ Projeto 1/scripts/1_digital_image_properts.py
@@ -0,0 +1,55 @@
+
+import numpy as np
+from PIL import Image
+from numpy import asarray
+
+# converting image to grayscale
+image = Image.open('../Images/nathigor.jpg').convert('L')
+image.show()
+
+def main():
+    # Open image
+
+    print(image.format)
+    print(image.size)
+    print(image.mode)
+    #print(image.bits)
+    print(image.getextrema())
+    # convert image to numpy array
+    npImage = np.array(image)
+    print(type(npImage))
+    # summarize shape
+    print(npImage.shape)
+
+    # value of pixel 0 0
+    print(npImage[0][0])
+    print(npImage[10][10])
+
+    # values os a windows 5 X 5
+    print(npImage[0:5, 0:5])
+
+    # divide by 2 pixels
+    npImage = (npImage / 2).astype(int)
+
+    # values os a windows 5 X 5
+    print(npImage[0:5, 0:5])
+
+    # invert the pixels
+    print("Inverting the pixels...")
+    x, y = npImage.shape
+    for i in range(x):
+        for j in range(y):
+            npImage[i,j] = abs(npImage[i,j] - 255)
+
+    #Convert ndarray image to Pillow image
+    image2 = Image.fromarray(npImage)
+    image2.show()
+
+
+    # create the histogram
+    histogram, bin_edges = np.histogram(npImage, bins=256, range=(0, 255))
+    print(histogram.shape)
+
+
+if __name__ == "__main__":
+    main()
