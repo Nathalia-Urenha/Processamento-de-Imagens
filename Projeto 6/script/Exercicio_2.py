e187aece971ded625894892577af584a428a3af4
Nathalia-Urenha <naty.urenha@gmail.com>
2021-09-27 15:46:48 -0300

    Filtros Passa-Baixa

--- Projeto 6/script/Exercicio_2.py
+++ Projeto 6/script/Exercicio_2.py
@@ -4,38 +4,38 @@ import matplotlib.pyplot as plt
 from math import sqrt, exp
 
 
-def distance(point1, point2):
+def distancia(point1, point2):
     return sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
 
-def idealFilterLP(D0, imgShape):
+def FiltroIdeal(D0, imgShape):
     base = np.zeros(imgShape[:2])
     rows, cols = imgShape[:2]
     center = (rows / 2, cols / 2)
     for x in range(cols):
         for y in range(rows):
-            if distance((y, x), center) < D0:
+            if distancia((y, x), center) < D0:
                 base[y, x] = 1
     return base
 
 
-def butterworthLP(D0, imgShape, n):
+def FiltroButterworth(D0, imgShape, n):
     base = np.zeros(imgShape[:2])
     rows, cols = imgShape[:2]
     center = (rows / 2, cols / 2)
     for x in range(cols):
         for y in range(rows):
-            base[y, x] = 1 / (1 + (distance((y, x), center) / D0) ** (2 * n))
+            base[y, x] = 1 / (1 + (distancia((y, x), center) / D0) ** (2 * n))
     return base
 
 
-def gaussianLP(D0, imgShape):
+def FiltroGaussiano(D0, imgShape):
     base = np.zeros(imgShape[:2])
     rows, cols = imgShape[:2]
     center = (rows / 2, cols / 2)
     for x in range(cols):
         for y in range(rows):
-            base[y, x] = exp(((-distance((y, x), center) ** 2) / (2 * (D0 ** 2))))
+            base[y, x] = exp(((-distancia((y, x), center) ** 2) / (2 * (D0 ** 2))))
     return base
 
 
@@ -50,13 +50,13 @@ img_fft = np.fft.fft2(img)
 img_fft_centralizada = np.fft.fftshift(img_fft)
 
 # b) a imagem de cada filtro;
-filtro_pb_ideal = idealFilterLP(50, img.shape)
+filtro_pb_ideal = FiltroIdeal(50, img.shape)
 plt.subplot(152), plt.imshow(np.abs(filtro_pb_ideal), "gray"), plt.title("Filtro Ideal")
 
-filtro_pb_Butterworth = butterworthLP(50, img.shape, 20)
+filtro_pb_Butterworth = FiltroButterworth(50, img.shape, 20)
 plt.subplot(153), plt.imshow(np.abs(filtro_pb_Butterworth), "gray"), plt.title("Filtro Butterworth")
 
-filtro_pb_Gaussiano = gaussianLP(50, img.shape)
+filtro_pb_Gaussiano = FiltroGaussiano(50, img.shape)
 plt.subplot(154), plt.imshow(np.abs(filtro_pb_Gaussiano), "gray"), plt.title("Filtro Gaussiano")
 
 plt.show()
